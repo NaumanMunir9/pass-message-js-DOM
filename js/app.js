94642802fb9f2e4fd3a7755f760c2ba6d18a4f38
Nauman Munir <naumanmunir@gmail.com>
2020-07-23 01:04:43 +0500

    final commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
 (function () {
   document.getElementById("message-form").addEventListener("submit", (e) => {
-    // prevent defult behaviour
     e.preventDefault();
-
-    // getting a value
     const message = document.getElementById("message");
     const value = message.value;
 
-    // check for empty value
-    if (value === "") {
-      const feeback = document.querySelector(".feedback");
-      feeback.classList.add("show");
+    if (message.value === "") {
+      document.querySelector(".feedback").classList.add("show");
       setTimeout(() => {
-        feeback.classList.remove("show");
+        document.querySelector(".feedback").classList.remove("show");
       }, 3000);
     } else {
-      // change value
       document.querySelector(".message-content").textContent = value;
       message.value = "";
     }
